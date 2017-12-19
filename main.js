a364cfbf32216cd9378112adf9314bb39ba6f527
William Eiffert <weiffert@purdue.edu>
2017-12-19 16:32:45 -0500

    Fixed more bugs

--- main.js
+++ main.js
@@ -133,30 +133,35 @@ function validJumpLocations(x, y) {
   x = parseInt(x);
   y = parseInt(y);
   let location = [];
-  let point = {
-    x: 0,
-    y: 0
-  };
+  
   
   if(isNotSameTokenType(x + 1, y - direction) && isSpaceNotTaken(x + 2, y - 2 * direction)) {
-    point.x = x + 2;
-    point.y = y - 2*direction;
+    let point = {
+      x: x + 2,
+      y: y - 2*direction
+    };
     location.push(point);
   }
   if(isNotSameTokenType(x - 1, y - direction) && isSpaceNotTaken(x - 2, y - 2 * direction)) {
-    point.x = x-2;
-    point.y = y-2*direction;
+    let point = {
+      x: x - 2,
+      y: y - 2*direction
+    };
     location.push(point);
   }
   if(isKing(x, y)) {
     if(isNotSameTokenType(x + 1, y + direction) && isSpaceNotTaken(x + 2, y + 2 * direction)) {
-      point.x = x + 2;
-      point.y = y + 2 * direction;
+      let point = {
+        x: x + 2,
+        y: y + 2*direction
+      };
       location.push(point);
     }
     if(isNotSameTokenType(x - 1, y + direction) && isSpaceNotTaken(x - 2, y + 2 * direction)) {
-      point.x = x-2;
-      point.y = y+2*direction;
+      let point = {
+        x: x - 2,
+        y: y + 2*direction
+      };
       location.push(point);
     }
   }
@@ -202,8 +207,8 @@ function isSpaceTaken(x, y) {
 
 function isNotSameTokenType(x, y) {
   let element = getElement(x, y);
-  if(element != null)
-   return element.children[0].className.indexOf(direction > 0 ? "two" : "one") == -1;
+  if(isSpaceTaken(x, y))
+    return !isSameTokenType(x, y);
   return false;
 }
 
